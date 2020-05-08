94645cb261ad1add093d43d644ed35505bfccda5
Padunk <abraham.agel@gmail.com>
2020-05-08 16:44:26 +0700

    adding responsive images

--- src/components/Home/Home.svelte
+++ src/components/Home/Home.svelte
@@ -1,7 +1,7 @@
 <style>
   .drop-filter {
-    backdrop-filter: blur(40px);
-    -webkit-backdrop-filter: blur(40px);
+    backdrop-filter: blur(90px);
+    -webkit-backdrop-filter: blur(90px);
   }
   /* .outer-text-container::before {
     content: '';
@@ -64,19 +64,21 @@
       title: '100Days Tracker App',
       description: 'An application to tracked your #100DaysOfSomething.',
       tech: ['ReactJS', 'TypeScript', 'Tailwind', 'NodeJS', 'PostgreSQL'],
-      imgPath: '/assets/images/one_hundred_days_tracker.png',
+      imgPath: '/assets/images/one_hundred_days_tracker',
+      imgType: 'png',
       videoPath: '/assets/videos/one_hundred_days_tracker.webm',
       videoType: 'webm',
       alt: 'one hundred days tracker app screenshot',
       url: 'http://onehundred-days-tracker.herokuapp.com/',
-      color: '#43419042'
+      color: '#b794f442'
     },
     {
       title: 'FIFA World Cup 2018',
       description:
         'An application to show 2018 FIFA World Cup Schedule and Results.',
       tech: ['ReactJS', 'CSS', 'PWA'],
-      imgPath: '/assets/images/2018_fifa_world_cup.png',
+      imgPath: '/assets/images/2018_fifa_world_cup',
+      imgType: 'png',
       videoPath: '/assets/videos/2018_fifa_world_cup.mp4',
       videoType: 'mp4',
       alt: '2018 FIFA World Cup App screenshot',
@@ -87,7 +89,8 @@
       title: 'Book Markdown Reader',
       description: 'An application to read book written in markdown in Github.',
       tech: ['ReactJS', 'TypeScript', 'React Transition Group', 'NodeJS'],
-      imgPath: '/assets/images/book_markdown_reader.png',
+      imgPath: '/assets/images/book_markdown_reader',
+      imgType: 'png',
       videoPath: '/assets/videos/book_markdown_reader.mp4',
       videoType: 'mp4',
       alt: 'book markdown reader app screenshot',
@@ -99,35 +102,38 @@
       description:
         'A Reddit clone application made with Redux as State Management',
       tech: ['ReactJS', 'Redux', 'NodeJS'],
-      imgPath: '/assets/images/reddit_clone.png',
+      imgPath: '/assets/images/reddit_clone-update',
+      imgType: 'png',
       videoPath: '/assets/videos/reddit_clone.mp4',
       videoType: 'mp4',
       alt: 'reddit clone app screenshot',
       url: 'https://react-redux-readable.web.app/',
-      color: '#20B2AA42'
+      color: '#02606142'
     },
     {
       title: 'Weather App',
       description:
         'A weather prediction application for cities around the world',
       tech: ['ReactJS', 'Weather API'],
-      imgPath: '/assets/images/react_weather_app.png',
+      imgPath: '/assets/images/react_weather_app',
+      imgType: 'png',
       videoPath: '/assets/videos/react_weather_app.mp4',
       videoType: 'mp4',
       alt: 'react weather app screenshot',
       url: 'https://react-weather-api.firebaseapp.com/',
-      color: '#FFA07A42'
+      color: '#f25c4942'
     },
     {
       title: 'Pomodoro Clock',
       description: 'Simple Pomodoro clock application',
       tech: ['ReactJS', 'HTML Canvas', 'Codepen'],
-      imgPath: '/assets/images/pomodoro_clock.png',
+      imgPath: '/assets/images/pomodoro_clock',
+      imgType: 'png',
       videoPath: '/assets/videos/pomodoro_clock.mp4',
       videoType: 'mp4',
       alt: 'book markdown reader app screenshot',
       url: 'https://codepen.io/padunk/full/awWEPY',
-      color: '#FFA51142'
+      color: '#db8c0a42'
     }
   ];
 
@@ -181,7 +187,7 @@
         class="hidden bottom-0 right-0 mr-16 mb-16 text-white text-5xl italic
         lg:absolute lg:block "
       >
-        <a href="{project.url}">
+        <a href="{project.url}" target="_blank" rel="noreferrer">
           <h2 style="{`color: ${project.color.slice(0, -2)}`}">
             {project.title}
           </h2>
@@ -198,12 +204,22 @@
         </video>
       </div> -->
       <section>
-        <img
-          src="{project.imgPath}"
-          alt="{project.alt}"
-          class="w-screen h-screen object-cover"
-          loading="lazy"
-        />
+        <picture>
+          <source
+            srcset="{project.imgPath + '-crop' + '.' + project.imgType}"
+            media="(min-width: 768px)"
+          />
+          <source
+            srcset="{project.imgPath + '.' + project.imgType}"
+            media="(min-width: 640px)"
+          />
+          <img
+            class="w-screen h-screen object-cover"
+            src="{project.imgPath + '-mobile' + '.' + project.imgType}"
+            alt="{project.alt}"
+            loading="lazy"
+          />
+        </picture>
       </section>
     </div>
   {/each}
