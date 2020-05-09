b64d5a531a5a1c53cd19eb935e7233910bce99cf
Padunk <abraham.agel@gmail.com>
2020-05-09 15:40:35 +0700

    updating UI

--- src/components/Home/Home.svelte
+++ src/components/Home/Home.svelte
@@ -3,139 +3,30 @@
     backdrop-filter: blur(90px);
     -webkit-backdrop-filter: blur(90px);
   }
-  /* .outer-text-container::before {
-    content: '';
-    shape-outside: radial-gradient(
-      farthest-side at bottom right,
-      transparent 100%,
-      red 0
-    );
-    float: left;
-    clear: left;
-    width: 7rem;
-    height: 7rem;
-  }
-  .inner-text-container::before {
-    content: '';
-    shape-outside: radial-gradient(
-      farthest-side at top right,
-      transparent 100%,
-      red 0
-    );
-    float: left;
-    clear: left;
-    width: 7rem;
-    height: 7rem;
+
+  #video {
+    width: 90vh;
+    height: 90vh;
+    box-shadow: 10px 10px 20px 0 rgba(0, 0, 0, 0.2);
+    will-change: opacity;
   }
 
-  .outer-text-container::after {
-    content: '';
-    shape-outside: radial-gradient(
-      farthest-side at bottom left,
-      transparent 100%,
-      red 0
-    );
-    float: right;
-    clear: right;
-    width: 7rem;
-    height: 7rem;
+  .active {
+    background-color: rgba(255, 255, 255, 0.7);
+    backdrop-filter: blur(30px);
+    -webkit-backdrop-filter: blur(30px);
   }
-  .inner-text-container::after {
-    content: '';
-    shape-outside: radial-gradient(
-      farthest-side at top left,
-      transparent 100%,
-      red 0
-    );
-    float: right;
-    clear: right;
-    width: 7rem;
-    height: 7rem;
-  } */
+
+  .video-active {
+    opacity: 1;
+  }
 </style>
 
 <script>
   import { onMount, beforeUpdate } from 'svelte';
 
   export let name;
-
-  const projectLists = [
-    {
-      title: '100Days Tracker App',
-      description: 'An application to tracked your #100DaysOfSomething.',
-      tech: ['ReactJS', 'TypeScript', 'Tailwind', 'NodeJS', 'PostgreSQL'],
-      imgPath: '/assets/images/one_hundred_days_tracker',
-      imgType: 'png',
-      videoPath: '/assets/videos/one_hundred_days_tracker.webm',
-      videoType: 'webm',
-      alt: 'one hundred days tracker app screenshot',
-      url: 'http://onehundred-days-tracker.herokuapp.com/',
-      color: '#b794f4'
-    },
-    {
-      title: 'FIFA World Cup 2018',
-      description:
-        'An application to show 2018 FIFA World Cup Schedule and Results.',
-      tech: ['ReactJS', 'CSS', 'PWA'],
-      imgPath: '/assets/images/2018_fifa_world_cup',
-      imgType: 'png',
-      videoPath: '/assets/videos/2018_fifa_world_cup.mp4',
-      videoType: 'mp4',
-      alt: '2018 FIFA World Cup App screenshot',
-      url: 'https://world-cup-pwa.firebaseapp.com/',
-      color: '#62afd2'
-    },
-    {
-      title: 'Book Markdown Reader',
-      description: 'An application to read book written in markdown in Github.',
-      tech: ['ReactJS', 'TypeScript', 'React Transition Group', 'NodeJS'],
-      imgPath: '/assets/images/book_markdown_reader',
-      imgType: 'png',
-      videoPath: '/assets/videos/book_markdown_reader.mp4',
-      videoType: 'mp4',
-      alt: 'book markdown reader app screenshot',
-      url: '#',
-      color: '#ededb4'
-    },
-    {
-      title: 'Reddit Clone',
-      description:
-        'A Reddit clone application made with Redux as State Management',
-      tech: ['ReactJS', 'Redux', 'NodeJS'],
-      imgPath: '/assets/images/reddit_clone-update',
-      imgType: 'png',
-      videoPath: '/assets/videos/reddit_clone.mp4',
-      videoType: 'mp4',
-      alt: 'reddit clone app screenshot',
-      url: 'https://react-redux-readable.web.app/',
-      color: '#00ced1'
-    },
-    {
-      title: 'Weather App',
-      description:
-        'A weather prediction application for cities around the world',
-      tech: ['ReactJS', 'Weather API'],
-      imgPath: '/assets/images/react_weather_app',
-      imgType: 'png',
-      videoPath: '/assets/videos/react_weather_app.mp4',
-      videoType: 'mp4',
-      alt: 'react weather app screenshot',
-      url: 'https://react-weather-api.firebaseapp.com/',
-      color: '#f25c49'
-    },
-    {
-      title: 'Pomodoro Clock',
-      description: 'Simple Pomodoro clock application',
-      tech: ['ReactJS', 'HTML Canvas', 'Codepen'],
-      imgPath: '/assets/images/pomodoro_clock',
-      imgType: 'png',
-      videoPath: '/assets/videos/pomodoro_clock.mp4',
-      videoType: 'mp4',
-      alt: 'book markdown reader app screenshot',
-      url: 'https://codepen.io/padunk/full/awWEPY',
-      color: '#db8c0a'
-    }
-  ];
+  export let projects;
 
   let alpha = (+'70').toString(16);
   let xOffset = 100;
@@ -154,7 +45,7 @@
     const { innerWidth } = window;
     if (innerWidth >= 768) {
       circleXOffset = 16;
-      xOffset = 70;
+      xOffset = 85;
       return;
     }
     circleXOffset = 14;
@@ -163,7 +54,7 @@
 
   function handleWheel(event) {
     if (event.deltaY > 0) {
-      if (idx === projectLists.length - 1) return;
+      if (idx === projects.length - 1) return;
       idx += 1;
     }
 
@@ -172,63 +63,70 @@
       idx -= 1;
     }
   }
+
+  let video;
+  let videoPlay = false;
+
+  function handleMouseOver(event) {
+    videoPlay = true;
+    video.src = event.target.dataset.videoTitle;
+
+    let playPromise = video.play();
+    if (playPromise !== undefined) {
+      playPromise
+        .then(_ => {
+          console.log('video is playing');
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
+  }
+
+  function handleMouseOut(event) {
+    videoPlay = false;
+    video.src = '';
+    video.pause();
+  }
 </script>
 
 <svelte:window on:wheel="{handleWheel}" on:resize="{handleResize}" />
 
 <h1 class="hidden">{name}</h1>
-<main class="w-screen h-screen relative">
-
-  {#each projectLists as project, i (project.title)}
+<main class=" w-screen h-screen relative">
+  {#each projects as project, i (project.title)}
     <div
       class="w-screen h-screen absolute inset-0 transition-all duration-1000"
       style="{`transform: translateX(${(idx - i) * xOffset}vw)`}"
     >
-      <section
-        class="hidden bottom-0 right-0 mr-16 mb-16 text-white text-5xl italic
-        lg:absolute lg:block "
-      >
-        <a href="{project.url}" target="_blank" rel="noreferrer">
-          <h2 style="{`color: ${project.color}`}">{project.title}</h2>
-        </a>
-      </section>
-      <!-- <div class="w-screen h-screen overflow-hidden">
-        <video class="w-screen h-screen object-cover" autoplay muted loop>
-          <source
-            poster="{project.imgPath}"
-            src="{project.videoPath}"
-            type="{project.videoType}"
-          />
-          Your browser is not supported!
-        </video>
-      </div> -->
-      <section>
-        <picture>
-          <source
-            srcset="{project.imgPath + '-crop' + '.' + project.imgType}"
-            media="(min-width: 768px)"
-          />
-          <source
-            srcset="{project.imgPath + '.' + project.imgType}"
-            media="(min-width: 640px)"
-          />
-          <img
-            class="w-screen h-screen object-cover"
-            src="{project.imgPath + '-mobile' + '.' + project.imgType}"
-            alt="{project.alt}"
-            loading="lazy"
-          />
-        </picture>
-      </section>
+      <picture>
+        <source
+          srcset="{project.imgPath + '-crop' + '.' + project.imgType}"
+          media="(min-width: 768px)"
+        />
+        <source
+          srcset="{project.imgPath + '.' + project.imgType}"
+          media="(min-width: 640px)"
+        />
+        <img
+          class="w-screen h-screen object-cover"
+          src="{project.imgPath + '-mobile' + '.' + project.imgType}"
+          alt="{project.alt}"
+          loading="lazy"
+          width="100%"
+          height="100%"
+        />
+      </picture>
+
     </div>
   {/each}
   <section
     class="rounded-full absolute w-56 h-56 right-0 bottom-0 mr-4 mb-4 text-xs
     z-10 overflow-hidden drop-filter shadow-2xl md:w-64 md:h-64 md:text-sm
     lg:left-0 lg:top-0 lg:mt-32 lg:ml-56"
-    style="{`background-color: ${projectLists[idx].color + alpha}`}"
+    style="{`background-color: ${projects[idx].color + alpha}`}"
   >
-    {#each projectLists as pro, index (pro.title)}
+    {#each projects as pro, index (pro.title)}
       <div
         class="absolute transition-all duration-500 pt-16 h-full rounded-full
         md:pt-20"
@@ -248,6 +146,43 @@
         </div>
       </div>
     {/each}
-
   </section>
+  <div
+    class="w-screen h-screen absolute z-20 flex items-center transition-all
+    duration-200"
+    class:active="{videoPlay}"
+  >
+    <video
+      class="object-scale-down rounded-full ml-64 shadow-2xl bg-gray-800
+      opacity-0 transition-opacity duration-200"
+      class:video-active="{videoPlay}"
+      id="video"
+      autoplay
+      muted
+      loop
+      preload="none"
+      type="video/mp4"
+      bind:this="{video}"
+    >
+      Your browser don't support video.
+    </video>
+  </div>
+  {#each projects as project, i (project.title)}
+    <section
+      class="hidden bottom-0 right-0 mr-16 mb-16 text-white text-5xl italic z-50
+      lg:absolute lg:block transition-all duration-1000"
+      style="{`transform: translateX(${(idx - i) * xOffset}vw)`}"
+    >
+      <a href="{project.url}" target="_blank" rel="noreferrer">
+        <h2
+          style="{`color: ${project.color}`}"
+          on:mouseover="{handleMouseOver}"
+          on:mouseout="{handleMouseOut}"
+          data-video-title="{project.videoPath}"
+        >
+          {project.title}
+        </h2>
+      </a>
+    </section>
+  {/each}
 </main>
