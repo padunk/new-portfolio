f11720174ca363f6cf42fe92a1a98976bee10577
Padunk <abraham.agel@gmail.com>
2020-05-16 12:46:57 +0700

    adding basic rules

--- src/Tailwindcss.svelte
+++ src/Tailwindcss.svelte
@@ -1,10 +1,25 @@
 <style global>
   @tailwind base;
+  html,
+  body {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
   body {
     padding: 0;
     overflow: hidden;
+    color: #333;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Lato', sans-serif;
   }
 
   @tailwind components;
+  .active {
+    display: none;
+    opacity: 0;
+  }
+  
   @tailwind utilities;
 </style>
