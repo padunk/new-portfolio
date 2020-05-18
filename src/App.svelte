d68fd5e5c1a3d79053d21e9286b2695619b58c8e
Padunk <abraham.agel@gmail.com>
2020-05-18 10:34:01 +0700

    adding page transitions.

--- src/App.svelte
+++ src/App.svelte
@@ -7,15 +7,29 @@
   import Home from './components/Home/Home.svelte';
   import About from './components/About/About.svelte';
   import AllProjects from './components/AllProjects/AllProjects.svelte';
+  import PageTransition from './PageTransition.svelte';
 
   export let projects;
   let name = 'Abraham Anak Agung';
+  
 </script>
 
 <Router>
   <Tailwindcss />
   <Navigation />
-  <Route path="/" component="{Home}" {name} {projects} />
-  <Route path="about" component="{About}" {name} />
-  <Route path="all" component="{AllProjects}" />
+  <Route path="/">
+    <PageTransition>
+      <Home {name} {projects} />
+    </PageTransition>
+  </Route>
+  <Route path="about">
+    <PageTransition>
+      <About {name} />
+    </PageTransition>
+  </Route>
+  <Route path="all">
+    <PageTransition>
+      <AllProjects />
+    </PageTransition>
+  </Route>
 </Router>
---
+++ src/PageTransition.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { blur, fly } from 'svelte/transition';
+  import { cubicOut, sineOut } from 'svelte/easing';
+
+  const height = window.innerHeight;
+</script>
+
+<div
+  in:fly="{{ delay: 550, duration: 1500, opacity: 1, easing: cubicOut, y: height }}"
+  out:blur="{{ duration: 700, opacity: 0, amount: 10, easing: sineOut }}"
+>
+  <slot />
+</div>
