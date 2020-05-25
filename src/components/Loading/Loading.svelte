0c2cd27f037ed7e5e4c28e6b2e74b44b7204c20c
Padunk <abraham.agel@gmail.com>
2020-05-25 11:34:05 +0700

    made new animation

--- src/components/Loading/Loading.svelte
+++ src/components/Loading/Loading.svelte
@@ -1,14 +1,51 @@
 <style>
-  .grid-wrapper {
-    display: grid;
-    grid-template-columns: repeat(10, minmax(20px, 1fr));
-    column-gap: 20px;
-    height: 40vh;
-    width: 70vw;
-  }
-
   .block {
     border-radius: 20px;
+    min-width: 20px;
+    width: 50px;
+    margin: 0 20px;
+    height: 300px;
+    animation: animate 1.4s linear infinite;
+  }
+
+  .block:nth-child(1) {
+    animation-delay: 0;
+  }
+  .block:nth-child(2) {
+    animation-delay: 0.2s;
+  }
+  .block:nth-child(3) {
+    animation-delay: 0.4s;
+  }
+  .block:nth-child(4) {
+    animation-delay: 0.6s;
+  }
+  .block:nth-child(5) {
+    animation-delay: 0.8s;
+  }
+  .block:nth-child(6) {
+    animation-delay: 1s;
+  }
+  .block:nth-child(7) {
+    animation-delay: 1.2s;
+  }
+
+  @keyframes animate {
+    0% {
+      box-shadow: 0 0 0 rgba(0, 0, 0, 0.5);
+      opacity: 0;
+      transform: translateX(-50px) scale(1);
+    }
+    50% {
+      box-shadow: 0 20px 50px rgba(0, 0, 0, 0.5);
+      opacity: 1;
+      transform: translateX(0px) scale(1.2);
+    }
+    100% {
+      box-shadow: 0 0 0 rgba(0, 0, 0, 0.5);
+      opacity: 0;
+      transform: translateX(50px) scale(1);
+    }
   }
 </style>
 
@@ -22,32 +59,14 @@
   onMount(() => {
     gsap.set('.invisible', { autoAlpha: 1 });
 
-    gsap
-      .timeline()
-      .from('.loading-letter', {
-        stagger: {
-          each: 0.25,
-          ease: 'power1.out'
-        },
-        opacity: 0,
-        repeat: -1
-      })
-      .to(
-        '.block',
-        {
-          y: -50,
-          boxShadow:
-            '10px 10px 20px 0 rgba(0, 0, 0, 0.2), -10px -10px 20px 0 rgba(255, 255, 255, ,0.1)',
-          repeat: -1,
-          yoyo: true,
-          stagger: {
-            from: 'center',
-            each: 0.3,
-            ease: 'power3.inOut'
-          }
-        },
-        '<'
-      );
+    gsap.timeline().from('.loading-letter', {
+      stagger: {
+        each: 0.25,
+        ease: 'power1.out'
+      },
+      opacity: 0,
+      repeat: -1
+    });
   });
 </script>
 
@@ -60,17 +79,14 @@
     </p>
     <p>{$loadingProgress} %</p>
   </div>
-  <div class="grid-wrapper m-auto">
-    <div class="block"></div>
-    <div class="block"></div>
-    <div class="block"></div>
-    <div class="block"></div>
-    <div class="block"></div>
-    <div class="block"></div>
-    <div class="block"></div>
-    <div class="block"></div>
-    <div class="block"></div>
-    <div class="block"></div>
+  <div class="m-auto flex justify-center items-center">
+    <span class="block bg-red-700"></span>
+    <span class="block bg-red-700"></span>
+    <span class="block bg-red-700"></span>
+    <span class="block bg-red-700"></span>
+    <span class="block bg-red-700"></span>
+    <span class="block bg-red-700"></span>
+    <span class="block bg-red-700"></span>
   </div>
 
 </div>
