c513b45943b532197000e9cc9ee2f7e1fc3929a2
Padunk <abraham.agel@gmail.com>
2020-06-04 12:18:44 +0700

    change video assets to cloudinary

--- src/App.svelte
+++ src/App.svelte
@@ -13,15 +13,15 @@
   let completeLoadingAssets = false;
 
   const allAssets = [
-    'https://res.cloudinary.com/padunk/video/upload/v1590378846/my_portfolio/book_markdown_reader_ncnhij.mp4',
-    'https://res.cloudinary.com/padunk/video/upload/v1590378827/my_portfolio/one_hundred_days_tracker_nmamjc.mp4',
-    'https://res.cloudinary.com/padunk/video/upload/v1590378825/my_portfolio/reddit_clone-update_wsjttd.mp4',
-    'https://res.cloudinary.com/padunk/video/upload/v1590378822/my_portfolio/2018_fifa_world_cup_elzxdh.mp4',
-    'https://res.cloudinary.com/padunk/video/upload/v1590378818/my_portfolio/react_weather_app_e9atiu.mp4',
-    'https://res.cloudinary.com/padunk/video/upload/v1590378815/my_portfolio/pomodoro_clock_upjlgj.mp4'
+    // 'https://res.cloudinary.com/padunk/video/upload/v1590378846/my_portfolio/book_markdown_reader_ncnhij.mp4',
+    // 'https://res.cloudinary.com/padunk/video/upload/v1590378827/my_portfolio/one_hundred_days_tracker_nmamjc.mp4',
+    // 'https://res.cloudinary.com/padunk/video/upload/v1590378825/my_portfolio/reddit_clone-update_wsjttd.mp4',
+    // 'https://res.cloudinary.com/padunk/video/upload/v1590378822/my_portfolio/2018_fifa_world_cup_elzxdh.mp4',
+    // 'https://res.cloudinary.com/padunk/video/upload/v1590378818/my_portfolio/react_weather_app_e9atiu.mp4',
+    // 'https://res.cloudinary.com/padunk/video/upload/v1590378815/my_portfolio/pomodoro_clock_upjlgj.mp4'
   ];
   projects.forEach(project => {
-    allAssets.push(project.imgPath + '.' + project.imgType, /* project.videoPath*/);
+    allAssets.push(project.imgPath + '.' + project.imgType, project.videoPath);
   });
   otherProjects.forEach(project => {
     allAssets.push(project.imgPath + '.' + project.imgType);
